fafaaac4d17f5ae6e54c5a941b981de72f2205db
Sebi <Sebi@DESKTOP-IN2IS25>
2024-02-09 13:13:50 +0200

    Add project files.

---
+++ eggs/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace eggs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtMiss = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::eggs.Properties.Resources.egg;
+            this.pictureBox1.Location = new System.Drawing.Point(71, 120);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "eggs";
+            // 
+            // Player
+            // 
+            this.Player.Image = global::eggs.Properties.Resources.chicken_normal;
+            this.Player.Location = new System.Drawing.Point(251, 448);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(85, 80);
+            this.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player.TabIndex = 1;
+            this.Player.TabStop = false;
+            this.Player.Click += new System.EventHandler(this.Player_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::eggs.Properties.Resources.egg;
+            this.pictureBox3.Location = new System.Drawing.Point(519, 120);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "eggs";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::eggs.Properties.Resources.egg;
+            this.pictureBox2.Location = new System.Drawing.Point(296, 120);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "eggs";
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(12, 9);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(59, 18);
+            this.txtScore.TabIndex = 4;
+            this.txtScore.Text = "Scor: 0";
+            // 
+            // txtMiss
+            // 
+            this.txtMiss.AutoSize = true;
+            this.txtMiss.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMiss.Location = new System.Drawing.Point(516, 9);
+            this.txtMiss.Name = "txtMiss";
+            this.txtMiss.Size = new System.Drawing.Size(86, 18);
+            this.txtMiss.TabIndex = 5;
+            this.txtMiss.Text = "Pierdute: 0";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.Main);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(614, 531);
+            this.Controls.Add(this.txtMiss);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Prinde ou";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtMiss;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
---
+++ eggs/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eggs
+{
+    public partial class Form1 : Form
+
+
+    {
+        bool goLeft, goRight;
+        int speed = 8;
+        int score = 0;
+        int missed = 0;
+
+        Random randX = new Random();
+        Random randY = new Random();
+        PictureBox splash = new PictureBox();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            RestartGame();
+        }
+
+        private void Player_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Main(object sender, EventArgs e)
+        {
+            txtScore.Text = "Scor: " + score;
+            txtMiss.Text = "Pierdute: " + missed;
+
+            if(goLeft==true && Player.Left>0)
+            {
+                Player.Left -= 12;
+                Player.Image = Properties.Resources.chicken_normal2;
+
+            }
+            if(goRight==true&& Player.Left+Player.Width<this.ClientSize.Width)
+            {
+                Player.Left += 12;
+                Player.Image = Properties.Resources.chicken_normal;
+            }
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox && (string)x.Tag=="eggs")
+                {
+                    x.Top += speed;
+
+                    if(x.Top+x.Height>this.ClientSize.Height)
+                    {
+                        splash.Image = Properties.Resources.splash;
+                        splash.Location = x.Location;
+                        splash.Height = 60;
+                        splash.Width = 60;
+                        splash.BackColor = Color.Transparent;
+
+                        this.Controls.Add(splash);
+
+
+
+                        x.Top = randY.Next(80, 300) * -1;
+                        x.Left = randX.Next(5, this.ClientSize.Width - x.Width);
+                        missed += 1;
+                        Player.Image = Properties.Resources.chicken_hurt;
+
+                    }
+                    if(Player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        x.Top = randY.Next(80, 300) * -1;
+                        x.Left = randX.Next(5, this.ClientSize.Width - x.Width);
+                        score += 1;
+                    }
+                }
+            }
+            if(score>10)
+            {
+                speed = 10;
+            }
+            if(missed>5)
+            {
+                GameTimer.Stop();
+                MessageBox.Show("Jocul a luat sfarsit! " + Environment.NewLine + " Apasa OK pentru a incerca iar");
+                RestartGame();
+            }
+               
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if(e.KeyCode==Keys.Right)
+            {
+                goRight = true;
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+        }
+
+
+
+        private void RestartGame()
+        {
+            foreach(Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag=="eggs")
+                {
+                    x.Top = randY.Next(80, 300) * -1;
+                    x.Left = randX.Next(5, this.ClientSize.Width - x.Width);
+                }
+            }
+            Player.Left = this.ClientSize.Width / 2;
+            Player.Image = Properties.Resources.chicken_normal;
+
+            score = 0;
+            missed = 0;
+            speed = 8;
+
+            goLeft = false;
+            goRight = false;
+
+            GameTimer.Start();
+        }
+    }
+}
